aff5f365ebfde78a585bfcf939468e94bb507a2b
Markius <markius160404@gmail.com>
2025-05-19 09:40:51 +0200

    Retoque para crear apk

--- app.config.js
+++ app.config.js
@@ -3,7 +3,18 @@ export default {
     name: 'RiverSpain',
     slug: 'riverspain',
     version: '1.0.0',
+    orientation: 'portrait',
+    android: {
+      package: 'com.markius.riverspain',
+      adaptiveIcon: {
+        foregroundImage: "./img/logo-azul.png",
+        backgroundColor: '#ffffff'
+      }
+    },
     extra: {
+      eas: {
+        projectId: '9ddc1cdf-b81a-4f24-9c3c-1b865a1e63a8',
+      },
       apiUrl: 'https://institutions-carriers-interpreted-explains.trycloudflare.com/riverspain', // Fallback por si no se carga .env
     },
   },
